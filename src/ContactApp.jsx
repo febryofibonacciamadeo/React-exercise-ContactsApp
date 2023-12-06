5afbf122233eee3f4cbdfcc7665dc74245edf9e9
febryofibonacciamadeo <febryofibonacciamadeo@gmail.com>
2023-12-06 13:35:08 +0700

    frynnn commit

---
+++ src/ContactApp.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import ContactList from "./ContactList";
+import { getData } from "../utils/data";
+import ContactInput from "./ContactInput";
+
+class ContactApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      contacts : getData(),
+    }
+
+    this.onDeleteHandler = this.onDeleteHandler.bind(this);
+    this.onAddContatcHandler = this.onAddContatcHandler.bind(this);
+  }
+
+  onDeleteHandler(id) {
+    const contacts = this.state.contacts.filter(contact => contact.id !== id);
+    this.setState({ contacts })
+  }
+
+  onAddContatcHandler({ name, tag }) {
+    this.setState((prevState) => {
+      return {
+        contacts : [
+          ...prevState.contacts,
+          {
+            id: +new Date(),
+            name,
+            tag,
+            imageUrl : '/images/default.jpg',
+          }
+        ]
+      }
+    });
+  }
+
+  render() {
+    return (
+      <div className="contact-app">
+        <h1>Aplikasi Kontak</h1>
+        <h2>Tambah Kontak</h2>
+        <ContactInput addContact={this.onAddContatcHandler}/>
+        <h2>Daftar Kontak</h2>
+        <ContactList contacts={this.state.contacts} onDelete={this.onDeleteHandler}/>
+      </div>
+    )
+  }
+}
+
+export default ContactApp;
